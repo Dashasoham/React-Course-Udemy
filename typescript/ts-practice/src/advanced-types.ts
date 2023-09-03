adf41f789e4efd4cee0997edca92a3deca8fe0e0
Daria Korniienko <106033233+Dashasoham@users.noreply.github.com>
2023-09-03 21:20:43 +0100

    generic function

---
+++ typescript/ts-practice/src/advanced-types.ts
@@ -0,0 +1,85 @@
+type Admin={
+    name:string;
+    privileges:string[];
+
+}
+
+type Employee={
+    name:string;
+    startDate:Date;
+
+}
+
+type ElevatedEmployee=Admin & Employee;
+
+const e1:ElevatedEmployee={name:'Max',privileges:['create-server'],startDate:new Date}
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+type Universal = Combinable & Numeric;
+console.log(2);
+
+
+
+function add(a:Combinable,b:Combinable){
+    if(typeof a==='string'||typeof b==='string'){
+        return a.toString()+b.toString();
+    }
+    return a+b;
+}
+
+type UnknownEmployee=Employee|Admin;
+
+
+
+function printEmployeeInformation(emp:UnknownEmployee){
+    console.log('Name: '+emp.name);
+    if('privileges' in emp){
+        console.log('Privileges: '+emp.privileges);
+    
+    }
+    if('startDate' in emp){
+        console.log('Date: '+emp.startDate);
+    }
+}
+printEmployeeInformation(e1);
+
+
+interface Bird{
+    type:'bird';
+    flyingSpeed:number;
+
+}
+
+interface Horse{
+    type:'horse';
+    runningSpeed:number;
+
+
+}
+
+type Animal=Bird|Horse;
+
+function moveAnimal(animal:Animal){
+    if ('flyingSpeed' in animal){
+    console.log('Moving with speed: '+animal.flyingSpeed);}
+}
+
+
+const userInputElement=document.getElementById('user-input')! as HTMLInputElement;
+
+userInputElement.value='Hi there!';
+
+
+
+interface ErrorContainer{//{email:string,username:string}
+    [prop:string]:string;
+}
+
+const errorBag:ErrorContainer={
+    email:'Not a valid email!',
+}
+
+
+
+

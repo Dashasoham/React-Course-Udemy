3bf581467b1f823deee98e1182b5023f1736e6e9
Daria Korniienko <106033233+Dashasoham@users.noreply.github.com>
2023-09-29 15:18:55 +0100

    creatin list

--- typescript/react-ts/src/components/ToDoList.tsx
+++ typescript/react-ts/src/components/ToDoList.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import './ToDoList.css'
+
 interface ToDoListProps{
-    items:{id:number,text:string}[]
+    items:{id:number,text:string}[];
+    deleteItem:(todoId:number)=>void
 }
 
 const ToDoList:React.FC<ToDoListProps>=(props)=>{
+
+const deleteToDo=(todoId:number)=>{
+    props.deleteItem(todoId)
+
+}
     return <ul>
-        {props.items.map(todo=><li key={todo.id}>{todo.text}</li>)}
+        {props.items.map(todo=><li key={todo.id}>
+            <span>{todo.text}</span>
+            <button onClick={()=>deleteToDo(todo.id)}>DELETE</button>
+
+            </li>)}
     </ul>
 }
 

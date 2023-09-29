14d013465a2b19edf9359894940889ccce99637c
Daria Korniienko <106033233+Dashasoham@users.noreply.github.com>
2023-09-29 15:17:29 +0100

    base file

--- typescript/react-ts/src/App.tsx
+++ typescript/react-ts/src/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ToDoList from "./components/ToDoList";
 import NewToDo from "./components/NewToDo";
+import { ToDo } from "./components/todo.model";
 
 const App:React.FC=()=> {
-  const todos=[{id:1,text:'Finish the course'}]
+const [todos,setTodos]=useState<ToDo[]>([])
 
+
+
+  const todoAddHandler=(enteredText:string)=>{
+    setTodos(prevTodos=>
+       [...prevTodos,{id:Math.random(),text:enteredText}]
+)}
+
+const ToDoDeleteHandler=(todoId:number)=>{
+  setTodos(prevTodos=>
+   prevTodos.filter(todo=>todo.id!==todoId)
+  )
+
+
+
+}
+  
   return (
     <div className="App">
-      <NewToDo/>
-   <ToDoList items={todos}/>
+      <NewToDo onAddToDo={todoAddHandler}/>
+   <ToDoList items={todos} deleteItem={ToDoDeleteHandler}/>
     </div>
   );
 }
